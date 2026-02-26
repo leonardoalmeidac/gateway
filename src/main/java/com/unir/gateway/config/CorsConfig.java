0d924f7f87c7e9d24b361f2808ca285898e4b7b0
Leonardo Almeida <59582766+leonardoalmeidac@users.noreply.github.com>
2026-02-25 22:09:54 -0500

    Add CORS configuration for web filter
    Configure CORS settings to allow specific origins and methods.

---
+++ src/main/java/com/unir/gateway/config/CorsConfig.java
@@ -0,0 +1,31 @@
+package com.unir.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        // Permitimos tanto local como producción
+        corsConfig.setAllowedOrigins(Arrays.asList(
+                "http://localhost:3000",
+                "https://front-relatos-de-papel-six.vercel.app"));
+        corsConfig.setMaxAge(3600L);
+        corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+        corsConfig.setAllowedHeaders(Arrays.asList("*"));
+        corsConfig.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsWebFilter(source);
+    }
+}
